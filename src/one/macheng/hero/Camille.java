a1da3d4a23c159575598293539035aa80c4eccb0
macheng2017 <gos360@163.com>
2020-03-05 10:47:14 +0800

    init lol hero

---
+++ src/one/macheng/hero/Camille.java
@@ -0,0 +1,20 @@
+package one.macheng.hero;
+
+public class Camille {
+    public void q() {
+        System.out.println("Q");
+    }
+
+    public void w() {
+        System.out.println("W");
+    }
+
+    public void e() {
+        System.out.println("E");
+    }
+
+    public void r() {
+        System.out.println("R");
+    }
+
+}
---
+++ src/one/macheng/hero/Irelia.java
@@ -0,0 +1,20 @@
+package one.macheng.hero;
+
+public class Irelia {
+    public void q() {
+        System.out.println("Q");
+    }
+
+    public void w() {
+        System.out.println("W");
+    }
+
+    public void e() {
+        System.out.println("E");
+    }
+
+    public void r() {
+        System.out.println("R");
+    }
+
+}
